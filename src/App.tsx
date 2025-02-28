d4a90b28ec1c0b7d9252ffcc259e356a797bea45
Etienne Koekemoer <me@etiennek.com>
2025-02-28 14:34:16 +0100

    Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { MilkdownProvider } from "@milkdown/react";
 
 import { GiHamburgerMenu } from "react-icons/gi";
+import { MdNoteAdd } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
 
 import Editor from "./components/Editor";
 import "./App.css";
@@ -9,22 +11,34 @@ function App() {
   return (
     <>
       <div className="drawer h-full">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <input id="main-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content h-full min-h-full">
           {/* Page content here */}
-          <label
-            htmlFor="my-drawer"
-            className="btn btn-soft drawer-button fixed m-2 z-1 opacity-50"
-          >
-            <GiHamburgerMenu />
-          </label>
+
+          <div className="fixed start-0 top-0 z-1">
+            <label
+              htmlFor="main-drawer"
+              className="btn btn-ghost drawer-button m-2 opacity-75 p-1 text-3xl text-primary"
+            >
+              <GiHamburgerMenu />
+            </label>
+
+            <button className="btn btn-ghost text-3xl p-1 m-2 opacity-75 text-primary">
+              <MdDeleteForever />
+            </button>
+          </div>
+
+          <button className="btn btn-ghost text-3xl p-1 fixed top-0 end-0 m-2 z-1 opacity-75 text-primary">
+            <MdNoteAdd />
+          </button>
+
           <MilkdownProvider>
             <Editor />
           </MilkdownProvider>
         </div>
         <div className="drawer-side z-2">
           <label
-            htmlFor="my-drawer"
+            htmlFor="main-drawer"
             aria-label="close sidebar"
             className="drawer-overlay"
           >

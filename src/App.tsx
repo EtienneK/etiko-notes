753fd125b955de5d982065e49a235def41db4b9b
Etienne Koekemoer <me@etiennek.com>
2025-02-28 18:35:04 +0100

    Trying to get persistance to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,136 @@
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
 import { MilkdownProvider } from "@milkdown/react";
 
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdNoteAdd } from "react-icons/md";
 import { MdDeleteForever } from "react-icons/md";
 
-import Editor from "./components/Editor";
+import Editor, { EditorRef } from "./components/Editor";
 import "./App.css";
 
+interface NoteWithoutText {
+  readonly id: string;
+  title: string;
+  lastModified: Date;
+}
+
+interface Note extends NoteWithoutText {
+  text: string;
+}
+
+interface NoteService {
+  save(note: Note): Promise<void>;
+  list(): Promise<NoteWithoutText[]>;
+  get(id: string): Promise<Note | null>;
+  search(text: string): Promise<Note[]>;
+  delete(id: string): Promise<void>;
+}
+
+interface NoteLocalStorage {
+  readonly id: string;
+  title: string;
+  text: string;
+  lastModified: string;
+}
+
+class LocalStorageNoteService implements NoteService {
+  async delete(id: string) {
+    localStorage.removeItem(this._key(id));
+  }
+
+  async search(text: string) {
+    return Object.keys(localStorage).map(id => {
+      const note = JSON.parse(localStorage.getItem(id)!) as NoteLocalStorage;
+      return {
+        id: note.id,
+        title: note.title,
+        text: note.text,
+        lastModified: new Date(note.lastModified)
+      };
+    }).filter(note => {
+      return note.text.toLowerCase().includes(text.toLowerCase()) || note.title.toLowerCase().includes(text.toLowerCase());
+    });
+  }
+
+  async get(id: string) {
+    const note = localStorage.getItem(this._key(id));
+    return note ? JSON.parse(note) : null;
+  }
+
+  async save(note: Note) {
+    const maxTitleLength = 50;
+    let title = note.text.split('\n')[0].replace(/^#+/, '').trim() || 'Untitled Note';
+    if (title.length > maxTitleLength) {
+      title = title.substring(0, maxTitleLength).trim() + '...';
+    }
+    note.title = title;
+    localStorage.setItem(this._key(note.id), JSON.stringify(note));
+  }
+
+  async list() {
+    return Object.keys(localStorage).map(id => {
+      const note = JSON.parse(localStorage.getItem(id)!) as NoteLocalStorage;
+      return {
+        id: note.id,
+        title: note.title,
+        lastModified: new Date(note.lastModified)
+      }
+    }).sort((a, b) => b.lastModified.getTime() - a.lastModified.getTime());
+  }
+
+  _key: (id: string) => string = (id) => `notes|${id}`;
+}
+
+const noteService = new LocalStorageNoteService();
+
+function createNote() {
+  const newNote = {
+    id: nanoid(),
+    title: 'Untitled Note',
+    text: '',
+    lastModified: new Date()
+  };
+  return newNote;
+}
+
 function App() {
+  const editorRef = React.useRef<EditorRef>(null);
+  const [notes, setNotes] = useState<NoteWithoutText[]>([]);
+  const [currentNote, setCurrentNote] = useState<Note | null>(null);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const notes = await noteService.list();
+      setNotes(notes);
+    };
+    fetchNotes();
+  }, []);
+
+  const handleCreateNote = () => {
+    const newNote = createNote();
+    setNotes([...notes, newNote]);
+    setCurrentNote(newNote);
+    noteService.save(newNote);
+    editorRef.current?.replaceAll(newNote.text, true);
+    editorRef.current?.focus();
+  };
+
+  function onMarkdownUpdated() {
+    return () => {
+      const markdown = editorRef.current?.getMarkdown();
+      console.log(currentNote);
+      if (currentNote && markdown) {
+        console.log(markdown);
+        const updatedNote = { ...currentNote, text: markdown, lastModified: new Date() };
+        setCurrentNote(updatedNote);
+        noteService.save(updatedNote);
+      }
+    }
+  };
+
+  console.log(currentNote);
+
   return (
     <>
       <div className="drawer h-full">
@@ -28,12 +151,15 @@ function App() {
             </button>
           </div>
 
-          <button className="btn btn-ghost text-3xl p-1 fixed top-0 end-0 m-2 z-1 opacity-75 text-primary">
+          <button
+            className="btn btn-ghost text-3xl p-1 fixed top-0 end-0 m-2 z-1 opacity-75 text-primary"
+            onClick={handleCreateNote}
+          >
             <MdNoteAdd />
           </button>
 
           <MilkdownProvider>
-            <Editor />
+            <Editor onMarkdownUpdated={onMarkdownUpdated()} ref={editorRef} />
           </MilkdownProvider>
         </div>
         <div className="drawer-side z-2">
@@ -45,12 +171,11 @@ function App() {
           </label>
           <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
             {/* Sidebar content here */}
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
+            {notes.map(note => (
+              <li key={note.id}>
+                <a>{note.title}</a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,5 +1,10 @@
+import React from 'react';
+
 import { Crepe } from "@milkdown/crepe";
 import { Milkdown, useEditor } from "@milkdown/react";
+import { listener, listenerCtx } from "@milkdown/kit/plugin/listener";
+import { replaceAll } from '@milkdown/kit/utils';
+import { editorViewCtx } from '@milkdown/kit/core'
 
 import "@milkdown/crepe/theme/common/style.css";
 // import "@milkdown/crepe/theme/nord.css";
@@ -25,12 +30,41 @@ Hello!
 
 `;
 
-function Editor() {
+export interface EditorRef {
+  getMarkdown(): string;
+  replaceAll: (markdown: string, flush: boolean) => void;
+  focus(): void;
+}
+
+export interface EditorProps {
+  ref: React.RefObject<EditorRef | null>;
+  onMarkdownUpdated: () => void;
+}
+
+function Editor(props: EditorProps) {
   useEditor((root) => {
     const crepe = new Crepe({
       root,
       defaultValue: markdown,
     });
+
+    if (props.ref) {
+      props.ref.current = {
+        focus: () => crepe.editor.ctx.get(editorViewCtx).focus(),
+        getMarkdown: () => crepe.getMarkdown(),
+        replaceAll: (markdown: string, flush: boolean = false) => crepe.editor.action(replaceAll(markdown, flush)),
+      };
+    }
+
+    crepe.editor
+      .config((ctx) => {
+        const listener = ctx.get(listenerCtx);
+        listener.updated(() => {
+          props.onMarkdownUpdated();
+        });
+      })
+      .use(listener);
+
     return crepe;
   }, []);
 
